0179f0c2d01332d0c3dc2b78813fa840f48751f3
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-09-21 17:17:41 +0700

    faet: Реализовать отправки данных пользователя на сервер

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,14 +1,46 @@
+import { useState } from 'react';
 
 export function  AuthForm(props) {
 
   const {title,name,buttonText,onSubmit,footer} = props;
 
+  const [dataUser,setDataUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  function resetDataUser () {
+    setDataUser({
+      email: '',
+      password: '',
+    });
+  }
+
+  // в объект записываются данные пользователя при регистрации емаил и пароль которые он вводит
+  function handleChangeInput(event){
+    const {name,value} = event.target;
+    setDataUser((prevDataUser) => ({
+      ...prevDataUser,
+      [name]:value
+    }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if(!dataUser.email || !dataUser.password) return;
+
+    onSubmit(dataUser) // отправляем данные которые ввел пользователь
+      .then(() => {
+        resetDataUser(); // очищаем поля
+      });
+  }
 
   return (
     <section className={`auth auth_type_${name}`}>
       <div className="auth__container">
       <h1 className="auth__title">{title}</h1>
-      <form className={`auth__form auth__form_type_${name}`} action="#" name={`auth-${name}`} noValidate onSubmit={onSubmit}>
+      <form className={`auth__form auth__form_type_${name}`} action="#" name={`auth-${name}`} noValidate onSubmit={handleSubmit}>
       <div className="auth__block">
         <input
           className='auth__input auth__input_type_email'
@@ -18,6 +50,9 @@ export function  AuthForm(props) {
           minLength="4"
           maxLength="40"
           required
+          autoComplete="username"
+          value={dataUser.email}
+          onChange={handleChangeInput}
         />
         <span className='auth__error'></span>
       </div>
@@ -30,6 +65,9 @@ export function  AuthForm(props) {
           minLength="8"
           maxLength="40"
           required
+          autoComplete="current-password"
+          value={dataUser.password}
+          onChange={handleChangeInput}
         />
         <span className='auth__error'></span>
       </div>
