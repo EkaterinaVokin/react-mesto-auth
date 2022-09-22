a85f0288cc7eac6a18ef917d6924051aab5db134
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-09-22 13:26:39 +0700

    feat: Добавить всплывающее меню в мобильной версии

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,43 @@
 import logoPath from '../images/logo.svg';
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
 export function Header(props) {
-  
-  const {actions} = props;
+  const { actions } = props;
+
+  const [isOpened, setIsOpened] = useState(false);
+
+  // обработчик открыть бургер меню
+  function toggleIsOpened() {
+    setIsOpened(!isOpened);
+  }
 
   return (
-    <header className="header page__header">
-      <div className="header__container">
-        <NavLink className="header__link" to="/">
-          <img className="header__logo" src={logoPath} alt="Логотип" />
-        </NavLink>
-        <div className="header__menu">
-           {actions}
+    <>
+      <header className="header page__header">
+        <div className="header__container">
+          <div className="header__controls">
+            <NavLink className="header__link" to="/">
+              <img className="header__logo" src={logoPath} alt="Логотип" />
+            </NavLink>
+
+            <div
+              className={`header__burger-menu ${isOpened ? 'header__burger-menu_opened' : ''}`}
+              onClick={toggleIsOpened}
+            >
+              <div className="header__burger-bar"></div>
+              <div className="header__burger-bar"></div>
+              <div className="header__burger-bar"></div>
+            </div>
+          </div>
+
+          <div className={`header__menu ${isOpened ? 'header__menu_mobile-opened' : ''}`}>
+            <div className="header__data">
+              {actions}
+              </div>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 }
